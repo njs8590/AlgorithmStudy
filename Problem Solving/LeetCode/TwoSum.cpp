0de3c9cc6827dce1af11fea8e0d6e0584256af68
jiseong <njs8590@naver.com>
2021-12-12 00:16:39 +0900

    Leetcode init 두 수의 합구하기

--- Problem Solving/LeetCode/TwoSum.cpp
+++ Problem Solving/LeetCode/TwoSum.cpp
@@ -22,7 +22,7 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> answer;
+        vector<int> answer; 
         for (int i = 0; i < nums.size() - 1; i++) {
             for (int j = i + 1; j < nums.size(); j++) {
                 if (nums[i] + nums[j] == target) {
@@ -32,7 +32,7 @@ public:
             }
         }
 
-        return answer;
+        return answer ;
     }
 };
 
